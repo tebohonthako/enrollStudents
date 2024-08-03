d81c46b8427fe1e2138224cb92dc62e094f935a4
tebohonthako <tjaynthako@gmail.com>
2024-08-03 23:00:55 +0200

    components and Student service added

--- Frontend/angular/src/app/app.module.ts
+++ Frontend/angular/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
 // import { DataTablesModule } from './data-tables.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
