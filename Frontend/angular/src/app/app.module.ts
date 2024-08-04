36b59a08d88d5de81e87e74ae885a9df8906fae6
tebohonthako <tjaynthako@gmail.com>
2024-08-04 09:53:53 +0200

    addStudent component fixed

--- Frontend/angular/src/app/app.module.ts
+++ Frontend/angular/src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddStudentComponent } from './components/add-student/add-student.component';
 import { StudentListComponent } from './components/student-list/student-list.component';
-// import { DataTablesModule } from './data-tables.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';  // for server requests 
+// import {DataTablesModule} from 'angular-datatables';  
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { StudentListComponent } from './components/student-list/student-list.com
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     // DataTablesModule
   ],
   providers: [],
---
+++ Frontend/angular/src/app/student.ts
@@ -0,0 +1,10 @@
+// purpose of this class is to map the
+// specified fields with
+// the fields of Spring entity class.
+
+export class Student {
+  student_id: number = 0;
+  student_name: string = '';
+  student_email: string = '';
+  student_branch: string ='';
+}
